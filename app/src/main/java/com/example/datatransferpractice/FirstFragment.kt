d902a05878484a37808a7df9656328de103ab2c2
stevey-sy <sinsy89@hanmail.net>
2021-03-25 21:05:09 +0900

    [EDIT] fragment result 사용하여 fragment 간 데이터 전달

--- app/src/main/java/com/example/datatransferpractice/FirstFragment.kt
+++ app/src/main/java/com/example/datatransferpractice/FirstFragment.kt
@@ -1,13 +1,12 @@
 package com.example.datatransferpractice
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
+import androidx.core.os.bundleOf
+import androidx.fragment.app.*
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 
@@ -15,8 +14,6 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
     private lateinit var button : Button
-    // activity 에 맞는 view model 객체 생성
-    val mainViewModel by activityViewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +23,11 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         button = view.findViewById(R.id.button)
 
         button.setOnClickListener {
-            // view model 객체에 데이터 선언
-            mainViewModel.data = "Hello"
+            setFragmentResult("requestKey", bundleOf("data" to "Hello"))
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
 
--- app/src/main/java/com/example/datatransferpractice/SecondFragment.kt
+++ app/src/main/java/com/example/datatransferpractice/SecondFragment.kt
@@ -7,23 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
 
     private lateinit var button : Button
-    val mainViewModel by activityViewModels<MainViewModel>()
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Toast.makeText(requireContext(), mainViewModel.data, Toast.LENGTH_SHORT).show()
+
+        setFragmentResultListener("requestKey") {
+                resultKey, bundle ->
+            val data = bundle.getString("data", "")
+            Toast.makeText(requireContext(), data, Toast.LENGTH_SHORT).show()
+        }
 
         button = view.findViewById(R.id.second_button)
 
         button.setOnClickListener {
+            setFragmentResult("requestKey", bundleOf("data" to "Hello"))
             findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
         }
     }
